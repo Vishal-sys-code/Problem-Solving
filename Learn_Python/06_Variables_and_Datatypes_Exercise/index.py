da08a18a08c7b0872607beb1a598d4d10030fa25
Vishal Pandey <68536727+Vishal-sys-code@users.noreply.github.com>
2024-11-30 15:25:21 +0530

    Create index.py

---
+++ Learn_Python/06_Variables_and_Datatypes_Exercise/index.py
@@ -0,0 +1,8 @@
+print('Variables & Datatypes - Exercise')
+#Create appropriate Variables for Item name, the price and how many you have in stock
+
+item_name = "iPhone-15 Pro Max"
+item_price = 999.99
+item_stock = 12
+is_in_inventory = True
+print(item_name, item_price, item_stock)
