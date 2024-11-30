11bb13bd0b5bfad179e49de413e8cd1c9a0d1901
Vishal Pandey <68536727+Vishal-sys-code@users.noreply.github.com>
2024-11-30 14:58:36 +0530

    Create assignments.py

---
+++ Learn_Python/03_Print_Statement_and_Programflow/assignments.py
@@ -0,0 +1,5 @@
+# I want you to write a print statements that creates some nails in the right place and then modify the use statement that we have here so that we also use the nails with the hammer.
+
+print("Create Hammer")
+print("Create Nails")
+print("Use Hammer on Nails")
