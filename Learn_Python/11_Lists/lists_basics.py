204fa3d613901187f4fa94f4017481f75df2fdb0
Vishal Pandey <68536727+Vishal-sys-code@users.noreply.github.com>
2024-11-30 17:10:10 +0530

    Create lists_basics.py

---
+++ Learn_Python/11_Lists/lists_basics.py
@@ -0,0 +1,13 @@
+friends = ['John', 'Michael', 'Terry', 'Eric', 'Graham']
+print("Lists: ", friends)
+print(friends[0])
+print(friends[1])
+print(friends[2])
+print(friends[3])
+print(friends[4])
+print(friends[2:4])
+print(friends[:4])
+print(friends[:])
+print(len(friends))
+print(friends.index('Eric'))
+print(friends.count('Eric'))
