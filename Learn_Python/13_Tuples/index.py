7858df9f29dcc490d2ff1cf7c70e70ae2c06f8b8
Vishal Pandey <68536727+Vishal-sys-code@users.noreply.github.com>
2024-12-01 15:55:01 +0530

    Create index.py

---
+++ Learn_Python/13_Tuples/index.py
@@ -0,0 +1,12 @@
+friends = ['Eric', 'John', 'Michael', 'Terry', 'Graham', 'TerryG', 'Brian']
+friends_tuple = ('Eric', 'John', 'Michael', 'Terry', 'Graham', 'TerryG', 'Brian')
+print(friends)
+print(friends_tuple)
+
+# Tuple cannot be changed when we created the first time that is fixed, if you want to change it then create another tuple and copy that tuple there.
+# Tuple is faster than regular lists.
+# Tuple is more memory efficient than regular lists.
+# Tuple is more secure than regular lists. Because it is immutable.
+
+print(friends[2:4])
+print(friends_tuple[2:4])
