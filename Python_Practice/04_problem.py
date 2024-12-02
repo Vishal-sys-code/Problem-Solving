3e7d4031029b0b49a5e3eeb5e484f54221efa6cb
Vishal Pandey <68536727+Vishal-sys-code@users.noreply.github.com>
2024-12-02 23:53:17 +0530

    Update 04_problem.py

--- Python_Practice/04_problem.py
+++ Python_Practice/04_problem.py
@@ -1,1 +1,5 @@
 # Write a program that will give you the sum of 3 digits
+num1 = int(input("Enter your first number: "))
+num2 = int(input("Enter your second number: "))
+num3 = int(input("Enter your third number: "))
+print(f"The sum of the three digits are: {num1 + num2 + num3}")
