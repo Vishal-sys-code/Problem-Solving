d08e7873e770fe123d4c3cde987f32b1f3700fa1
Vishal Pandey <68536727+Vishal-sys-code@users.noreply.github.com>
2024-12-03 16:47:55 +0530

    Create loops.py

---
+++ Learn_Python/21_Loops/loops.py
@@ -0,0 +1,10 @@
+"""
+while continue:
+    code
+    iterator
+
+Three loop questions:
+1. What do I want to repeat.
+2. What do I want to change each time.
+3. How long it should keep repeating.
+"""
